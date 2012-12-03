644495c9375d4699960a7f88a30fea060e1decbf
Craig Williams <cwilliams.allancraig@gmail.com>
2012-12-03 15:27:31 -0700

    Minor cleanup

--- lib/tangerine/backlot/base.rb
+++ lib/tangerine/backlot/base.rb
@@ -1,19 +1,18 @@
 class Tangerine::Base
 
   def initialize(options={})
-    options.delete('content_type')
+    options.delete('content_type') if options['content_type']
+
     options.each do |k,v|
-      # TODO: Look into using Crack gem for reliably undercoring these
       attr = k.to_s.underscore
       method = :"#{attr}="
 
-      self.send(method, v) if self.class.method_defined?(method)
+      send(method, v) if self.class.method_defined?(method)
     end
   end
 
   def self.prepare_items(items)
-    # Yes, we are fully aware that type-checking sucks. Move along now, nothing to see here.
-    items.is_a?(Array) ? items : [items]
+    Array( items )
   end
 
   def self.finder(&block)
@@ -23,12 +22,12 @@ class Tangerine::Base
   def self.find(embed_code)
     results = prepare_items @finder.call(embed_code)
     found_item = results.select { |item| item['embedCode'] == embed_code }.first
-    self.new(found_item)
+    new(found_item)
   end
 
   def self.all
     items = prepare_items @finder.call
-    items.collect { |item| self.new(item) }
+    items.collect { |item| new(item) }
   end
 
 end
