9839c6f52187db4fd4904397819a89ea604fd395
Anthony Navarre & Ken Seal <pair+anthony.navarre+ken.seal@factorylabs.com>
2013-06-03 17:09:16 -0600

    Expire VCR cassettes & ignore URI params
    - Expire VCR Cassettes every 7 days
    - Don't expire VCR Cassettes if &expires or &signature differ in request URI

--- spec/support/vcr_support.rb
+++ spec/support/vcr_support.rb
@@ -1,5 +1,12 @@
 def pause_vcr(cassette_name=nil, erb=false)
-  VCR.insert_cassette(cassette_name, :erb => erb, record: :new_episodes)
+  VCR.insert_cassette cassette_name,
+    :erb => erb,
+    record: :new_episodes,
+    re_record_interval: 7.days,
+    :match_requests_on => [
+      :method,
+      VCR.request_matchers.uri_without_params(:expires, :signature)
+    ]
 end
 
 def play_vcr
